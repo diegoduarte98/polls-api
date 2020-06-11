6eaa18d2f2e3936f675de6d71ea0fbf7191ed3f1
Diego Duarte <ive_dduarte@uolinc.com>
2020-06-11 00:52:45 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+* [Polling app similar to twitter polls](https://www.callicoder.com/spring-boot-spring-security-jwt-mysql-react-app-part-1/)
+
---
+++ src/main/java/com/example/pollsapi/PollsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApiApplication.class, args);
+	}
+
+}
